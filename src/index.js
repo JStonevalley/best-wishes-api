a27e0195e43dd7d7f782aaf639fa9da44034187b
Jonas Stendahl <jonas.stendahl@outlook.com>
2018-11-20 08:09:37 +0100

    Fetch and save wish lists

--- src/index.js
+++ src/index.js
@@ -10,12 +10,9 @@ r = require('rethinkdb')
 const app = express()
 app.use(bodyParser.json())
 
-
-const USER_TABLE = process.env.USER_TABLE
-
 const connectionP = (process.env.IS_OFFLINE === 'true'
-  ? r.connect({ host: 'localhost', port: 28015 })
-  : r.connect({ host: 'localhost', port: 28015 })
+? r.connect({ host: 'localhost', port: 28015 })
+: r.connect({ host: 'localhost', port: 28015 })
 ).then(async (conn) => {
   await conn.use(process.env.ENV)
   return conn
@@ -37,8 +34,9 @@ const userValidation = yup.object().shape({
   email: yup.string().email().required(),
 })
 
+const USER_TABLE = process.env.USER_TABLE
+
 app.get('/user/:email', async ({params: {email}}, res) => {
-  console.log(email)
   try {
     userValidation.validateSync({email})
   } catch (error) {
@@ -46,7 +44,7 @@ app.get('/user/:email', async ({params: {email}}, res) => {
   }
   const conn = await connectionP
   try {
-    const data = r.table(USER_TABLE).get(email).run(conn)
+    const data = await r.table(USER_TABLE).get(email).run(conn)
     data
       ? res.json(data)
       : res.status(404).json({ error: "User not found" })
@@ -70,10 +68,63 @@ app.post('/user', async (req, res) => {
   try {
     const {first_error} = await r.table(USER_TABLE).insert(newUser).run(conn)
     if (first_error && first_error.startsWith('Duplicate primary key')) throw new yup.ValidationError('User already exists', newUser, 'email')
+    if (first_error) throw new Error(first_error)
     res.json(newUser)
   } catch (error) {
     console.error(error)
+    res.status(400).json({ error: 'Clould not save user'})
+  }
+})
+
+const WISH_LIST_TABLE = process.env.WISH_LIST_TABLE
+
+const wishListValidation = yup.object().shape({
+  id: yup.string().notRequired(),
+  title: yup.string().required(),
+  owner: yup.string().email().required(),
+  wishes: yup.array().of(yup.object().shape({
+    title: yup.string().required(),
+    body: yup.string().notRequired(),
+    link: yup.string().notRequired(),
+    image: yup.string().notRequired()
+  }))
+})
+
+app.get('/wish-list', async ({query: {email}}, res) => {
+  try {
+    userValidation.validateSync({email})
+  } catch (error) {
     res.status(400).json(error)
+  }
+  const conn = await connectionP
+  try {
+    const data = await (await r.table(WISH_LIST_TABLE).filter({owner: email}).run(conn)).toArray()
+    data
+      ? res.json(data)
+      : res.status(404).json({ error: "Wish list ot found" })
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Could not get wish lists' })
+  }
+})
+
+app.put('/wish-list', async (req, res) => {
+  const wishlist = req.body
+  try {
+    wishListValidation.validateSync(wishlist)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+  const conn = await connectionP
+  try {
+    const {first_error} = wishlist.id
+      ? await r.table(WISH_LIST_TABLE).update(wishlist).run(conn)
+      : await r.table(WISH_LIST_TABLE).insert(wishlist).run(conn)
+    if (first_error) throw new Error(first_error)
+    res.json(wishlist)
+  } catch (error) {
+    console.error(error)
+    res.status(400).json({ error: 'Could not save wish list'})
   }
 })
   
