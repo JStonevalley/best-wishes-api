22c4434e080fe962aebb2510d679f8ea27f7d1cc
Jonas Stendahl <jonas.stendahl@outlook.com>
2023-09-06 21:33:26 +0200

    Add interface for context

--- src/email/render.ts
+++ src/email/render.ts
@@ -3,7 +3,13 @@ import { compile } from 'handlebars'
 import { readFileSync } from 'fs'
 import path from 'path'
 
-export const renderEmailTemplate = (templateName: string, context) => {
+interface ShareEmailContext {
+  fromEmail: string
+  wishListTitle: string
+  wishListLink: string
+}
+
+export const renderEmailTemplate = (templateName: string, context: ShareEmailContext) => {
   // eslint-disable-next-line no-undef
   const templateString = readFileSync(path.resolve(__dirname, `./templates/${templateName}.mjml`)).toString('utf8')
   const { html } = mjml2html(compile(templateString)(context))
