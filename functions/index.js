15c5bafb91b069e47b0cc398336e3f1bdce3c7d0
Jonas Stendahl <jonas.stendahl@outlook.com>
2023-09-26 13:05:38 +0200

    Remove everything apart from pageMetaData parser

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,1 @@
 export { fetchPageMetadata } from './pageMetadata.js'
-export {
-  associateCreatedShareByEmail,
-  associateShareWithCreatedUserByEmail
-} from './dbTriggers.js'
-export { sendShareEmail } from './email/email.js'
